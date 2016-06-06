d2ad839872e83bc5e3574f49e99db50db1052dae
tharindu99 <madushanka.best@gmail.com>
2016-06-06 23:27:07 +0530

    device list reading from json file

---
+++ data/devices.js
@@ -0,0 +1,39 @@
+var json = 
+ {
+ "availableFields":[
+	 {
+        iconClass: 'diagram-node-start-icon',
+        label: 'Start',
+        type: 'start'
+      },
+      {
+        iconClass: 'diagram-node-task-icon',
+        label: 'Task',
+        type: 'task'
+      },
+      {
+        iconClass: 'diagram-node-state-icon',
+        label: 'State',
+        type: 'state'
+      },
+      {
+        iconClass: 'diagram-node-join-icon',
+        label: 'Join',
+        type: 'join'
+      },
+      {
+        iconClass: 'diagram-node-fork-icon',
+        label: 'Fork',
+        type: 'fork'
+      },
+      {
+        iconClass: 'diagram-node-condition-icon',
+        label: 'Condition',
+        type: 'condition'
+      },
+      {
+        iconClass: 'diagram-node-end-icon',
+        label: 'End',
+        type: 'end'
+      }
+ ]};
