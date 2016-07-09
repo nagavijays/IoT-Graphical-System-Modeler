9065a513c11091d4ab877dc62f601f0a84bd3076
tharindu99 <madushanka.best@gmail.com>
2016-07-10 00:39:45 +0530

    palate images modifiered

--- jointjs/palate.js
+++ jointjs/palate.js
@@ -1,3 +1,11 @@
-function palate_devices() {
-	
+function palate_devices(category,i) {
+	var device_cat  = document.getElementById("device_cat"+category);
+	var nw_elmnt = document.createElement('div');
+		nw_elmnt.setAttribute("class", "col-lg-3 col-md-4 col-xs-6");
+	var device_img = document.createElement('img');	
+		device_img.setAttribute("id","device_img");
+		device_img.setAttribute("src","./resources/"+device_catlog[i].image);
+		device_img.setAttribute("title",device_catlog[i].device);
+		nw_elmnt.appendChild(device_img);
+		device_cat.appendChild(nw_elmnt);
 }
