ca7059afa7675c0e3350f2755bab1ebd6dc39481
tharindu99 <madushanka.best@gmail.com>
2016-07-11 16:04:58 +0530

    element customize configure for class variable

--- jointjs/palate.js
+++ jointjs/palate.js
@@ -3,7 +3,7 @@ function palate_devices(category,i) {
 	var nw_elmnt = document.createElement('div');
 		nw_elmnt.setAttribute("class", "col-lg-3 col-md-4 col-xs-6");
 	var device_img = document.createElement('img');	
-		device_img.setAttribute("id","device_img");
+		device_img.setAttribute("class","device_img");
 		device_img.setAttribute("src","./resources/"+device_catlog[i].image);
 		device_img.setAttribute("title",device_catlog[i].device);
 		nw_elmnt.appendChild(device_img);
