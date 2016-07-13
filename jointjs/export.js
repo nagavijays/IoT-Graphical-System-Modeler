9b67fcea6aa0ee8337ec83252ee957fb6eb900a9
tharindu99 <madushanka.best@gmail.com>
2016-07-13 14:59:53 +0530

    meaningful export function implementation started

--- jointjs/export.js
+++ jointjs/export.js
@@ -1,8 +1,14 @@
 function export_graph(a){
         	var myWindow = window.open();
-        	console.log(a.toJSON());
         	myWindow.document.write("<title>Export</title><pre id='jsonExport'>h</pre>");
         	
         	myWindow.document.getElementById("jsonExport").innerHTML = JSON.stringify(a.toJSON(), undefined, 2);
         	
         }
+
+function export_sementic(a) {
+	var exp_grp = a.toJSON();
+	
+	
+	console.log(exp_grp.cells.length);
+}
