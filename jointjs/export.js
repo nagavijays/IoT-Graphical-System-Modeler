87ef61e64a06fdff19c3ba47bbc6bd3c2e79335e
tharindu99 <madushanka.best@gmail.com>
2016-07-27 15:24:27 +0530

    code cleaning

--- jointjs/export.js
+++ jointjs/export.js
@@ -1,9 +1,7 @@
 function export_graph(a){
         	var myWindow = window.open();
         	myWindow.document.write("<title>Export Graph</title><pre id='jsonExport'>h</pre>");
-        	
         	myWindow.document.getElementById("jsonExport").innerHTML = JSON.stringify(a.toJSON(), undefined, 2);
-        	
         }
 
 function export_sementic(a) {
